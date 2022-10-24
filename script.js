fcc3be27b0f1b760ce7b39665d15a3af281a1dcd
vsiva253 <n170450@rguktn.ac.in>
2022-10-24 15:20:20 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+fetch('https://fakestoreapi.com/products')
+            .then(data=>{
+                return data.json()
+            })
+            
+            .then(completedata=>{
+            
+                    let data1="";
+                    completedata.map(values=>{
+                        data1+=`<div class="card">
+                        <h1 class="title">${values.title}</h1>
+                        <img src=${values.image} alt="img" class="images">
+                        <p>${values.description}</p>
+                        <p class="category">${values.Category}</p>
+                        <p class="price">${values.price} </p>
+                        </div>`
+                     })
+                    document.getElementById("cards").innerHTML=data1;
+ })
